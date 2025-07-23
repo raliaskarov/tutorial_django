66807ffff6c3d038a3918d93b64452a325b4a4be
raliaskarov <r.aliaskarov@gmail.com>
2025-07-23 05:35:31 -0400

    interim save

--- final_project/tfjzl-final-cloud-app-with-database/onlinecourse/views.py
+++ final_project/tfjzl-final-cloud-app-with-database/onlinecourse/views.py
@@ -121,7 +121,6 @@ def submit(request, course_id):
     return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result',
                                 args=(course_id, submission_id)))
 
-# An example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
    submitted_anwsers = []
    for key in request.POST:
@@ -131,14 +130,26 @@ def extract_answers(request):
            submitted_anwsers.append(choice_id)
    return submitted_anwsers
 
-
-# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
-# you may implement it based on the following logic:
-        # Get course and submission based on their ids
-        # Get the selected choice ids from the submission record
-        # For each selected choice, check if it is a correct answer or not
-        # Calculate the total score
 def show_exam_result(request, course_id, submission_id):
+    context = {}
+    crouse = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
 
+    total_score = 0
+    questions = course.question_set.all()
+
+    for question in questions:
+        correct_choices = question.choice_set.filter(is_correct=True)
+        selected_choices = choices.filter(question=question)
+
+        if set(correct_choices) == set(selected_choices):
+            total_score += question.grade
+
+        context['course'] = course
+        context['grade'] = total_score
+        context['choices'] = choices
+
+        return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
